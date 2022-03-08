e55aea938c021f6411c5b11502cbd7460dafd317
simpleCalculus <60369311+simpleCalculus@users.noreply.github.com>
2022-03-08 21:32:42 +0500

    Update exercise-loops-and-functions.go

--- go tour/exercise-loops-and-functions.go
+++ go tour/exercise-loops-and-functions.go
@@ -19,3 +19,11 @@ func main() {
 	fmt.Printf("Sqrt: %f\n", Sqrt(802))
 	fmt.Printf("math.Sqrt: %f\n", math.Sqrt(802))
 }
+
+/*
+ If you are interested in the details of the algorithm, the z² − x above is 
+ how far away z² is from where it needs to be (x), and the division by 2z is 
+ the derivative of z², to scale how much we adjust z by how quickly z² is 
+ changing. This general approach is called Newton's method. It works well 
+ for many functions but especially well for square root.
+*/
